09c5d2a3f8df1296db4b404539526df7239bf302
Redargiel <dominik-gr@seznam.cz>
2024-02-29 22:54:40 +0100

    Update

--- Schizo Defense/Assets/Scripty/UIInterface.cs
+++ Schizo Defense/Assets/Scripty/UIInterface.cs
@@ -19,38 +19,46 @@ public class UIInterface : MonoBehaviour
             {
                 return;
             }
-            focusObs = Instantiate(gatlingTower, hit.point, gatlingTower.transform.rotation);
-            focusObs.GetComponent<Collider>().enabled = false;
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (!Physics.Raycast(ray, out hit))
-            {
-                return;
-            }
-            focusObs.transform.position = hit.point;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (!Physics.Raycast(ray, out hit))
-            {
-                return;
-            }
 
-            if (hit.collider.gameObject.CompareTag("platform") && hit.normal.Equals(new Vector3(0,1,0)))
+            // Check if there is already a tower on the platform
+            if (hit.collider.gameObject.CompareTag("platform") && hit.collider.gameObject.transform.childCount == 0)
             {
-                hit.collider.gameObject.tag = "occupied";
-                focusObs.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, focusObs.transform.position.y, hit.collider.gameObject.transform.position.z);
+                Vector3 center = hit.collider.bounds.center;
+                center.y = gatlingTower.transform.position.y; // Align with tower's Y position
+                focusObs = Instantiate(gatlingTower, center, gatlingTower.transform.rotation);
+                focusObs.GetComponent<Collider>().enabled = false;
             }
-            else
+        }
+        else if (Input.GetMouseButton(0) && focusObs != null)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
             {
-                Destroy(focusObs);
+                focusObs.transform.position = hit.collider.bounds.center;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0) && focusObs != null)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.CompareTag("platform"))
+                {
+                    focusObs.transform.position = hit.collider.bounds.center;
+                    hit.collider.gameObject.tag = "occupied"; // Assign "occupied" tag to the block
+                }
+                else
+                {
+                    Destroy(focusObs);
+                }
             }
             focusObs = null;
         }
     }
 }
+
+
+
+
