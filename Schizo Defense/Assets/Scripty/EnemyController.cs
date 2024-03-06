2c70b158a419722e9dc32da1872687970a023ada
Redargiel <dominik-gr@seznam.cz>
2024-03-06 17:45:22 +0100

    Hra opravena, healthbar přidán, vypadá to lit

--- Schizo Defense/Assets/Scripty/EnemyController.cs
+++ Schizo Defense/Assets/Scripty/EnemyController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
@@ -15,6 +16,12 @@ public class EnemyController : MonoBehaviour
 
     private bool wayPointSet = false;
 
+    public Slider healthBarPrefab;
+
+    Slider healthBar;
+
+    public int maxHealth = 100;
+
     NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -22,6 +29,11 @@ public class EnemyController : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         levelManager = FindObjectOfType<LevelManager>(); // Najde se LevelManager ve scénì
+
+        healthBar = Instantiate(healthBarPrefab, this.transform.position, Quaternion.identity);
+        healthBar.transform.SetParent(GameObject.Find("Canvas").transform);
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
     }
 
     // Update is called once per frame
@@ -31,12 +43,17 @@ public class EnemyController : MonoBehaviour
         {
             return;
         }
+        if (healthBar)
+        {
+            healthBar.transform.position = Camera.main.WorldToScreenPoint(this.transform.position + Vector3.up * 2f);
+        }
         if (!agent.pathPending && agent.remainingDistance <= agentStoppingDistance)
         {
             if (currentWayPointIndex == wayPoints.Count)
             {
                 levelManager.EnemyDestroyed();
                 Destroy(this.gameObject, 0.1f);
+                Destroy(healthBar.gameObject, 0.1f);
             }
             else
             {
@@ -50,5 +67,18 @@ public class EnemyController : MonoBehaviour
     {
         this.wayPoints = wayPoints;
         wayPointSet = true;
+    }
+
+    public void Hit(int damage)
+    {
+        if (healthBar)
+        {
+            healthBar.value -= damage;
+            if (healthBar.value <= 0)
+            {
+                Destroy(healthBar.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
